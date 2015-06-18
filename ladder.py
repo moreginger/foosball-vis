a8e3f62e4c7c9226e78a69292fd20262e54930ab
Tim Moore <tmm@corefiling.com>
2015-06-18 17:07:40 +0100

    Slight improvement to prediction by providing number of games played to magicnet. Tweaks to visuals etc.

--- ladder.py
+++ ladder.py
@@ -4,18 +4,27 @@ import urllib2
 import re
 import datetime
 import json
-from games import Game, games
-
-class GamesEncoder(json.JSONEncoder):
-
-    def default(self, obj):
-        if isinstance(obj, Game):
-            return {'time' : obj.time, 'blue': { 'name' : obj.blue_player, 'score': obj.blue_score }, 'red': { 'name': obj.red_player, 'score': obj.red_score } }
+from random import shuffle
+import games
+from collections import Counter
 
 
 def main():
     print("Content-type: application/json\n")
-    print(GamesEncoder().encode(list(reversed(list(games())[-1000:]))))
+
+    games_by_player = Counter()
+    def game_data(g):
+        blue = g.blue_player
+        red = g.red_player
+        games_by_player[blue] += 1
+        games_by_player[red] += 1
+        return {'time': g.time, 'blue': { 'name' : blue, 'score': g.blue_score, 'game': games_by_player[blue] }, 'red': { 'name': g.red_player, 'score': g.red_score, 'game': games_by_player[red] } }
+
+    data = [game_data(x) for x in games.games()][-5000:]
+    shuffle(data)
+    players = set([name for sublist in [[g['red']['name'], g['blue']['name']] for g in data] for name in sublist])
+    players = [{'name': p, 'games': games_by_player[p]} for p in players]
+    print(json.JSONEncoder().encode({'games': data, 'players': players}))
 
 
 if __name__ == '__main__':
