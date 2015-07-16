af1012c790b14882352456a2d88bde7c64e97645
Tim Moore <tmm@corefiling.com>
2015-07-16 10:57:15 +0100

    Player's line now continues 60 days minus lead in before retirement. This makes chart more consistent with https://github.com/jamesremuscat/TNTFL-web and the lead in gives a better presentation for retirees.

--- rankings.py
+++ rankings.py
@@ -4,6 +4,7 @@ import urllib2
 import re
 import datetime
 import json
+from collections import defaultdict
 from shapely.geometry import LineString
 from games import Game, games
 
@@ -27,31 +28,35 @@ class Ranking:
 
 class Player:
 
-    def __init__(self, name, lead_in_seconds):
+    def __init__(self, name, retire_seconds, lead_in_seconds):
         self.name = name
+        self.retire_seconds = retire_seconds
         self.lead_in_seconds = lead_in_seconds
         self.elo = 0.0
-        self.games = 0
         self.most_recent_game = None
-        self.active = False
         self.rankings = []
 
     def game(self, delta, time):
-        self.active = True
         self.elo += delta
-        self.games += 1
+        self.most_recent_game = time
 
+    def is_active(self, time):
+        return time - self.most_recent_game < self.retire_seconds
 
-    def update_ranking(self, ranking, time, inactivate=False):
-        ranking_changed = self.rankings and self.rankings[-1] and self.rankings[-1].ranking != ranking
+    def is_retired(self):
+        return self.rankings[-1].ranking == -1
+
+    def update_ranking(self, ranking):
+        ranking_changed = self.rankings and self.rankings[-1] and self.rankings[-1].ranking != ranking.ranking
+
         if ranking_changed:
             # Extra data point for pretty lines up/down
-            extra_ranking = Ranking(time - self.lead_in_seconds, self.rankings[-1].ranking)
+            extra_ranking = Ranking(ranking.time - self.lead_in_seconds, self.rankings[-1].ranking)
             popped = None
-            if self.rankings and self.rankings[-1] and self.rankings[-1].time > extra_ranking.time and self.rankings[-1].ranking != ranking:
+            if self.rankings and self.rankings[-1].ranking != -1 and self.rankings[-1].time > extra_ranking.time and self.rankings[-1].ranking != ranking.ranking:
                 popped = self.rankings.pop(-1)
 
-                if not self.rankings or not self.rankings[-1]:
+                if not self.rankings:
                     # Popped the last point
                     # First approximation: put it back
                     # TODO: Move the line back according to imagined projection back?
@@ -60,7 +65,7 @@ class Player:
                     # There is a previous point, intersect lines
                     previous = self.rankings[-1]
                     line1 = LineString([(previous.time, previous.ranking), (popped.time, popped.ranking)])
-                    line2 = LineString([(extra_ranking.time + (extra_ranking.time - time), extra_ranking.ranking + (extra_ranking.ranking - ranking)), (time, ranking)])
+                    line2 = LineString([(extra_ranking.time + (extra_ranking.time - ranking.time), extra_ranking.ranking + (extra_ranking.ranking - ranking.ranking)), (ranking.time, ranking.ranking)])
                     geom = line1.intersection(line2)
                     # TODO improve test!
                     extra_ranking = Ranking(geom.x, geom.y) if geom.geom_type is 'Point' else None
@@ -68,15 +73,20 @@ class Player:
             if extra_ranking:
                 self.rankings.append(extra_ranking)
 
-        if ranking_changed or inactivate or not self.rankings or not self.rankings[-1]:
-            self.rankings.append(Ranking(time, ranking))
+        if ranking_changed or not self.rankings:
+            self.rankings.append(ranking)
 
-        if inactivate:
-            self.active = False
-            self.rankings.append(None)
+    def retire(self, time):
+        # TODO Should intersect this really
+        retirement_rank = self.rankings[-1].ranking
+        while self.rankings and self.rankings[-1].ranking != -1 and self.rankings[-1].time > time:
+            self.rankings.pop(-1)
+
+        self.rankings.append(Ranking(time, retirement_rank))
+        self.rankings.append(Ranking(time, -1))
 
     def write_final_rank(self, time):
-        if self.active and self.rankings[-1]:
+        if self.is_active(time) and self.rankings[-1]:
             self.rankings.append(Ranking(time, self.rankings[-1].ranking))
 
     def __str__(self):
@@ -90,11 +100,10 @@ class Analysis:
 
     def __init__(self, importance = 25, retire_days = 60, lead_in_seconds = 16 * 60 * 60, teams = {}):
         self.importance = importance
-        self.retire_days = retire_days
+        self.retire_seconds = retire_days * day_seconds
         self.lead_in_seconds = lead_in_seconds
         self.player_to_team = dict((v, k) for k in teams for v in teams[k])
         self.players = {}
-        self.games = []
 
     def get_player(self, name):
         if self.player_to_team:
@@ -102,37 +111,41 @@ class Analysis:
         if name in self.players:
             return self.players[name]
         else:
-            player = self.players[name] = Player(name, self.lead_in_seconds)
+            player = self.players[name] = Player(name, self.retire_seconds, self.lead_in_seconds)
             return player
 
-    def game_played(self, new_game):
-        self.process_games(time = new_game.time)
+    def game_played(self, game):
+        players_by_retirement = defaultdict(set)
+        for retired_player in [p for p in self.players.values() if not p.is_active(game.time) and not p.is_retired()]:
+            players_by_retirement[retired_player.most_recent_game].add(retired_player)
 
-        self.games.append(new_game)
-        self.get_player(new_game.blue_player).most_recent_game = new_game.time
-        self.get_player(new_game.red_player).most_recent_game = new_game.time
+        for most_recent_game in sorted(players_by_retirement.keys()):
+            time = most_recent_game + self.retire_seconds
+            retired_players = players_by_retirement[most_recent_game]
+            for retired_player in retired_players:
+                retired_player.retire(time - self.lead_in_seconds)
+            retired_player_names = set([p.name for p in retired_players])
+            self.update_rankings(time, sorted([p for p in self.players.values() if p.is_active(time) and p.name not in retired_player_names], key=lambda x: x.elo, reverse=True))
+
+        blue = self.get_player(game.blue_player)
+        red = self.get_player(game.red_player)
+
+        predict = 1 / (1 + 10 ** ((red.elo - blue.elo) / 180))
+        result = game.blue_score / (game.blue_score + game.red_score)
+        delta = self.importance * (result - predict)
+
+        blue.game(delta, game.time)
+        red.game(-delta, game.time)
+
+        self.update_rankings(game.time, sorted([p for p in self.players.values() if p.is_active(game.time)], key=lambda x: x.elo, reverse=True))
+
+    def update_rankings(self, time, ranked_players):
+        rank = 1
+        for player in ranked_players:
+            player.update_ranking(Ranking(time, rank))
+            rank +=1
 
     def process_games(self, time=None, flush=False):
-        while self.games and (flush or time - self.games[0].time >= self.retire_days * day_seconds):
-            game = self.games.pop(0)
-            blue = self.get_player(game.blue_player)
-            red = self.get_player(game.red_player)
-
-            predict = 1 / (1 + 10 ** ((red.elo - blue.elo) / 180))
-            result = game.blue_score / (game.blue_score + game.red_score)
-            delta = self.importance * (result - predict)
-
-            blue.game(delta, game.time)
-            red.game(-delta, game.time)
-
-            players_by_rank = sorted([p for p in self.players.values() if p.active], key=lambda x: x.elo, reverse=True)
-
-            rank = 1
-            for player in players_by_rank:
-                inactivate = not flush and player.most_recent_game == game.time
-                player.update_ranking(rank, game.time, inactivate=inactivate)
-                rank += 1
-
         if flush:
             for player in self.players.values():
                 player.write_final_rank((datetime.datetime.now() - __epoch__).total_seconds())
@@ -144,7 +157,7 @@ class RankingsEncoder(json.JSONEncoder):
         if isinstance(obj, Player):
             return {'label': obj.name, 'elo': '{0:.3f}'.format(obj.elo), 'data': obj.rankings, 'last': obj.most_recent_game * 1000}
         if isinstance(obj, Ranking):
-            return [obj.time * 1000, obj.ranking]
+            return [obj.time * 1000, obj.ranking] if obj.ranking != -1 else None
         else:
             return super(FootballJsonEncoder, self).default(obj)
 
