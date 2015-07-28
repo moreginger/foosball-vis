a1103d9bb78e8375f19190e837b859a030a679ac
Tim Moore <moreginger@warpmail.net>
2015-07-28 18:01:22 +0100

    Use live data from TNTFL rather than static games file.

--- flow2/src/scripts/main.js
+++ flow2/src/scripts/main.js
@@ -2,12 +2,26 @@
 // you can require() bower components too!
 var prep = require("./prepare-data.js");
 var plot = require("./plot-data.js");
-var games = require("./games.js");
 
-var data = games.games();
-var elements = prep.prepareData(data);
-var container = document.getElementById('chart');
-plot.plotData(container , elements);
+var url = 'https://www.int.corefiling.com/~tmm/TNTFL-web/games.cgi?view=json&from=' + Math.floor(Date.now() / 1000 - 60 * 24 * 60 * 60);
+console.log(url);
+var request = new XMLHttpRequest();
+request.open('GET', url, true);
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400) {
+    var data = JSON.parse(request.responseText);
+    var elements = prep.prepareData(data);
+    var container = document.getElementById('chart');
+    plot.plotData(container , elements);
+  }
+  else {
+    console.log(request)
+  }
+};
+request.onerror = function() {
+  console.log(request)
+};
+request.send();
 
 // example.welcome();
 // var _ = require("underscore");
