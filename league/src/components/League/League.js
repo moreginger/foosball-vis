aa4b1888a4a906c5f45e4e427d155ca04267003f
tmm <tmm@dhcp47.int.corefiling.com>
2015-12-11 20:42:07 +0000

    Introduce LeagueTable component.

--- league/src/components/League/League.js
+++ league/src/components/League/League.js
@@ -1,15 +1,23 @@
 import React, { PropTypes, Component } from 'react';
 import { Nav, NavItem } from 'react-bootstrap';
+import LeagueTable from '../LeagueTable';
 
 class League extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      tab: 1
+      tab: 1,
+      data: [{
+        name: 'tmm',
+        points: 100
+      }, {
+        name: 'tlr',
+        points: 101
+      }]
     };
   }
-
+  
   handleSelect = (selectedKey) => {
     this.setState({
       tab: selectedKey
@@ -18,10 +26,13 @@ class League extends Component {
 
   render() {
     return (
-      <Nav bsStyle='tabs' activeKey={this.state.tab} onSelect={this.handleSelect}>
-        <NavItem eventKey={1}>Current</NavItem>
-        <NavItem eventKey={2}>All Time</NavItem>
-      </Nav>
+      <div>
+        <Nav bsStyle='tabs' activeKey={this.state.tab} onSelect={this.handleSelect}>
+          <NavItem eventKey={1}>Current</NavItem>
+          <NavItem eventKey={2}>All Time</NavItem>
+        </Nav>
+        <LeagueTable data={this.state.data}/>
+      </div>
     );
   }
 }
